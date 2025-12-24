1c8b21ebeecb9c34d8b50547a6bff093d3985040
Ramniwas Gupta <ramniwas.nitrr15@gmail.com>
2025-12-24 23:02:58 +0530

    order api updated

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -67,15 +67,15 @@ export const verifyTestOrder = async (req, res) => {
 
     // send order confirmation email
     await sendOrderConfirmationEmail({
-      email: "ramniwasg96@gmail.com",
-      customerName: "Ram Niwas",
+      email: req.user.email || "ramniwasg96@gmail.com",
+      customerName: req.user.name || "Ram Niwas",
       orderId: razorpay_order_id,
-      items: [
+      items: order.items.length > 0 ? order.items.length : [
         { title: "Wireless Headphones", quantity: 1, price: 2999 },
         { title: "Gaming Mouse", quantity: 2, price: 1499 },
       ],
-      totalAmount: 5997,
-      shippingAddress: "221B Baker Street, New Delhi, India",
+      totalAmount: order.amount || 4999,
+      shippingAddress: order.shippingAddress || "#234, Peer Nagar, Ghazipur, Uttar Pradesh, India - 233001",
     });
 
     res.json({ success: true, order })
--- routes/orderPayment.js
+++ routes/orderPayment.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { createTestOrder, verifyTestOrder } from "../controllers/orderController.js";
+import { authorizeRoles, protect } from "../middlewares/auth.js";
 
 const router = express.Router();
 
-router.post('/create-order', createTestOrder);
-router.post('/verify-payment', verifyTestOrder)
+router.post('/create-order', protect, authorizeRoles('admin','seller', 'user'), createTestOrder);
+router.post('/verify-payment', protect, authorizeRoles('admin','seller', 'user'), verifyTestOrder)
 
 export default router;
